7a909fa6608233d9552cc0657fe0a8ac0aa5fa0a
wilbobraggins <wilbobraggins@gmail.com>
2019-06-27 06:06:36 +0000

    origins update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'https://www.alexisbellydance.com/contact.html'
+     origins 'https://www.alexisbellydance.com/contact.html', 'https://www.alexisbellydance.com'
 
      resource '*',
        headers: :any,
